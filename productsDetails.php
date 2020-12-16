524b2a4a965c2589e3e13baa10507dbd3b60dd26
sushiljadhav <67819154+sushiljadhav@users.noreply.github.com>
2020-12-16 21:58:20 +0530

    modify productsDetails Page

--- productsDetails.php
+++ productsDetails.php
@@ -16,9 +16,9 @@
 <section class="products_details">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-3">
+			<div class="col-xl-5 col-md-5">
 			<div class="row">
-				<div class="col-md-1">
+				<div class="col-md-2">
 					<ul class="row products_images_slider">
 						<li>
 							<figure>
@@ -27,28 +27,29 @@
 						</li>
 						<li>
 							<figure>
-								<img src="assets/image/vegitable/vegi_two.jpg alt="Vegitable two">
+								<img src="assets/image/vegitable/vegi_two.jpg" alt="Vegitable two">
 							</figure>
 						</li>
 						<li>
 							<figure>
-							<img src="assets/image/vegitable/vegi_three.jpg alt="Vegitable three">
+							<img src="assets/image/vegitable/vegi_three.jpg" alt="Vegitable three">
 							</figure>
 						</li>
 						<li>
 							<figure>
-							<img src="assets/image/vegitable/vegi_Four.jpg alt="Vegitable Four">
+							<img src="assets/image/vegitable/vegi_Four.jpg" alt="Vegitable Four">
 							</figure>
 						</li>
 					</ul>
 				</div>
-				<div class="col-md-2">
+				<div class="col-md-10 col-xl-10 ">
 					<figure>
 						<img src="assets/image/vegitable/vegi_one.jpg" alt="Vegitable One">
 					</figure>
 					<div class="row products_details_button">
 						<div class="col">
 							<button class="btn btn-primary">
+							<i class="fa fa-shopping-cart" aria-hidden="true"></i>
 								Add To cart
 							</button>
 						</div>
@@ -60,15 +61,65 @@
 					</div>
 				</div>
 			</div>
-				
 			</div>
-			<div class="col-md-9">
+			<div class="col-xl-7 col-md-7 ">
+				<h5>Fresho</h3>
+				<h3>Fresho Onion - Medium/Kanda- Madhyam, 1 kg</h3>
+				<span class="products_price"><span class="original_price">MRP:Rs 61.25</span> <span class="discount_price">Price:Rs 49</span> You Save:20%(Inclusive of all taxes)</span>
+				<div class="form-group">
+					<label class="sr-only" for="inlineFormInputGroup">Username</label>
+					<div class="input-group mb-2">
+						<input type="Number" class="form-control form-control-sm" id="inlineFormInputGroup" placeholder="1" min="1">
+					</div>
+				</div>
+				<p class="products_details_info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quis eius quibusdam earum perspiciatis at ullam iusto ipsum incidunt sit!</p>
+				<button class="btn btn-primary"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Add to cart</button>
 			</div>
 		</div>
 	</div>
 
 </section>
 
+<section class="products_details_accordion">
+	<div class="container">
+	<h2>Fresho Onion - Medium/Kanda- Madhyam</h2>
+	<div class="accordion" id="accordionExample">
+  <div class="card">
+    <div class="card-header d-flex align-items-baseline" id="headingOne">
+			<h6>About the Product</h6>
+			<i class="fa fa-plus btn btn-link" type="button"  data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"></i>
+    </div>
 
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header d-flex align-items-baseline" id="headingTwo">
+			<h6>Nutritional Facts</h6>
+			<i class="fa fa-plus btn btn-link collapsed" type="button"  data-toggle="collapse" data-target="#collapseTwo" aria-expanded=false" aria-controls="collapseTwo"></i>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header d-flex align-items-baseline" id="headingThree">
+			<h6>Benefits</h6>
+			<i class="fa fa-plus btn btn-link collapsed" type="button"  data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"></i>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
+	</div>
+</section>
 
 <?php include("footer.php")?>
