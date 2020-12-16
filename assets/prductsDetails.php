4e102ef6758f386700d5ccbe6f826e98e4dd1b11
sushiljadhav <67819154+sushiljadhav@users.noreply.github.com>
2020-12-16 20:57:11 +0530

    Add bread Crumbs

---
+++ assets/prductsDetails.php
@@ -0,0 +1,25 @@
+<?php include("header.php")?>
+
+<div class="breadcrumb_content">	
+		<div class="container">
+			<div aria-label="breadcrumb">
+				<ol class="breadcrumb">
+					<li class="breadcrumb-item"><a href="#">Home</a></li>
+					<li class="breadcrumb-item"><a href="#">Library</a></li>
+					<li class="breadcrumb-item active" aria-current="page">Data</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+
+
+<section class="products_details">
+	<div class="container">
+		
+	</div>
+
+</section>
+
+
+
+<?php include("footer.php")?>
