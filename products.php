de281cad4ef4b31f97ba305aad3e144d96af8599
sushiljadhav <67819154+sushiljadhav@users.noreply.github.com>
2020-12-16 20:41:52 +0530

    Change file name

---
+++ products.php
@@ -0,0 +1,486 @@
+<?php include('header.php')?>
+<!--Product Details Page start Here-->
+<main>
+<!--Breadcrumb div start here -->
+<div class="breadcrumb_content">	
+	<div class="container">
+		<div aria-label="breadcrumb">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item"><a href="#">Home</a></li>
+				<li class="breadcrumb-item"><a href="#">Library</a></li>
+				<li class="breadcrumb-item active" aria-current="page">Data</li>
+			</ol>
+		</div>
+	</div>
+</div>
+
+<!-- Product Details section start here-->
+<section class="all_products">
+	<div class="container">
+			<div class="row">
+				<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 product_details_filter">
+					<div class="filter_content">
+					<ul class="list-group">
+						<li class="list-group-item">
+							<h5>Categories</h5>
+							<hr>
+							<h6>Fruits & vegetables</h6>
+							<ul class="list-group">
+								<li class="list-group-item"><a href="#FIXME" title="Apples"><span>Apples</span></a></li>
+								<li class="list-group-item"><a href="#FIXME" title="Apples"><span>Apples</span></a></li>
+								<li class="list-group-item"><a href="#FIXME" title="Apples"><span>Apples</span></a></li>
+								<li class="list-group-item"><a href="#FIXME" title="Apples"><span>Apples</span></a></li>
+								<li class="list-group-item"><a href="#FIXME" title="Apples"><span>Apples</span></a></li>
+							</ul>
+						</li>
+					</ul>
+					<ul class="list-group">
+						<li class="list-group-item">
+							<h5>Brand</h5>
+							<hr>
+							<div class="form-group search">
+							<input type="text" name="" id="" placeholder="search" class="form-control form-control-sm">
+							<i class="fa fa-search" aria-hidden="true"></i>	
+							</div>
+							<ul class="list-group checkbox_list">
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">bb Combo</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Farmogo</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Fresho</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Toda</label>
+							</li>
+								
+							</ul>
+						</li>
+					</ul>
+					<ul class="list-group">
+						<li class="list-group-item">
+							<h5>Seasonal</h5>
+							<hr>
+							<ul class="list-group checkbox_list">
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Pooja Item</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Summer Fruits</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Summer Veggi</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Winter Fruits</label>
+							</li>
+								
+							</ul>
+						</li>
+					</ul>
+					<ul class="list-group">
+						<li class="list-group-item">
+							<h5>Price</h5>
+							<hr>
+							<ul class="list-group checkbox_list">
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">less than Rs 20</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">less than Rs 50</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">less than Rs 200</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">More than Rs 500</label>
+							</li>
+								
+							</ul>
+						</li>
+					</ul>
+					<ul class="list-group">
+						<li class="list-group-item">
+							<h5>Discount</h5>
+							<hr>
+							<ul class="list-group checkbox_list">
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">less than Rs 20</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">less than Rs 50</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">less than Rs 200</label>
+							</li>
+							<li class="form-check form-check-inline">
+								<input type="checkbox" class="form-check-input form-control-sm" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">More than Rs 500</label>
+							</li>
+								
+							</ul>
+						</li>
+					</ul>
+					</div>
+				</div>
+				<div class="col-xl-10 col-lg-10 col-md-10 col-sm-10">
+					<p>Product details</p>	
+					<div class="row">
+						<div class="col-xl-3 col-md-3 col-lg-3 col-sm-3">
+							<a href="#FIXME" title="Strawberry">
+								<div class="card_content">
+									<figure>
+										<img src="assets/image/vegitable/vegi_one.jpg" alt="Vegitable One">
+									</figure>
+									<h6>strawberry</h6>
+									<p>Lorem ipsum dolor sit amet.</p>
+									<div class="form-group">
+										<select id="inputState" class="form-control form-control-sm">
+											<option selected>Choose...</option>
+											<option>500g - Rs.17.50</option>
+											<option>1kg - Rs.33.00</option>
+											<option>2Kg - Rs.66.00</option>
+										</select>
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="inlineFormInputGroup">Username</label>
+										<div class="input-group mb-2">
+											<div class="input-group-prepend">
+												<div class="input-group-text form-control-sm" style="font-size:14px">Qty</div>
+											</div>
+											<input type="Number" class="form-control form-control-sm" id="inlineFormInputGroup" placeholder="1">
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xl-6 col">
+											<div class="btn btn-primary btn-sm view_details">
+												Details
+											</div>
+										</div>
+										<div class="col-xl-6 col">
+										<div class="btn btn-primary btn-sm ">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										Add
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>	
+						<div class="col-xl-3 col-md-3 col-lg-3 col-sm-3">
+							<a href="#FIXME" title="Strawberry">
+								<div class="card_content">
+									<figure>
+										<img src="assets/image/vegitable/vegi_two.jpg" alt="Vegitable One">
+									</figure>
+									<h6>strawberry</h6>
+									<p>Lorem ipsum dolor sit amet.</p>
+									<div class="form-group">
+										<select id="inputState" class="form-control form-control-sm">
+											<option selected>Choose...</option>
+											<option>500g - Rs.17.50</option>
+											<option>1kg - Rs.33.00</option>
+											<option>2Kg - Rs.66.00</option>
+										</select>
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="inlineFormInputGroup">Username</label>
+										<div class="input-group mb-2">
+											<div class="input-group-prepend">
+												<div class="input-group-text form-control-sm" style="font-size:14px">Qty</div>
+											</div>
+											<input type="Number" class="form-control form-control-sm" id="inlineFormInputGroup" placeholder="1">
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xl-6 col">
+											<div class="btn btn-primary btn-sm view_details">
+												Details
+											</div>
+										</div>
+										<div class="col-xl-6 col">
+										<div class="btn btn-primary btn-sm ">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										Add
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xl-3 col-md-3 col-lg-3 col-sm-3">
+							<a href="#FIXME" title="Strawberry">
+								<div class="card_content">
+									<figure>
+										<img src="assets/image/vegitable/vegi_three.jpg" alt="Vegitable One">
+									</figure>
+									<h6>strawberry</h6>
+									<p>Lorem ipsum dolor sit amet.</p>
+									<div class="form-group">
+										<select id="inputState" class="form-control form-control-sm">
+											<option selected>Choose...</option>
+											<option>500g - Rs.17.50</option>
+											<option>1kg - Rs.33.00</option>
+											<option>2Kg - Rs.66.00</option>
+										</select>
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="inlineFormInputGroup">Username</label>
+										<div class="input-group mb-2">
+											<div class="input-group-prepend">
+												<div class="input-group-text form-control-sm" style="font-size:14px">Qty</div>
+											</div>
+											<input type="Number" class="form-control form-control-sm" id="inlineFormInputGroup" placeholder="1">
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xl-6 col">
+											<div class="btn btn-primary btn-sm view_details">
+											Details
+											</div>
+										</div>
+										<div class="col-xl-6 col">
+										<div class="btn btn-primary btn-sm ">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										Add
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xl-3 col-md-3 col-lg-3 col-sm-3">
+							<a href="#FIXME" title="Strawberry">
+								<div class="card_content">
+									<figure>
+										<img src="assets/image/vegitable/vegi_four.jpg" alt="Vegitable One">
+									</figure>
+									<h6>strawberry</h6>
+									<p>Lorem ipsum dolor sit amet.</p>
+									<div class="form-group">
+										<select id="inputState" class="form-control form-control-sm">
+											<option selected>Choose...</option>
+											<option>500g - Rs.17.50</option>
+											<option>1kg - Rs.33.00</option>
+											<option>2Kg - Rs.66.00</option>
+										</select>
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="inlineFormInputGroup">Username</label>
+										<div class="input-group mb-2">
+											<div class="input-group-prepend">
+												<div class="input-group-text form-control-sm" style="font-size:14px">Qty</div>
+											</div>
+											<input type="Number" class="form-control form-control-sm" id="inlineFormInputGroup" placeholder="1">
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xl-6 col">
+											<div class="btn btn-primary btn-sm view_details">
+											 Details
+											</div>
+										</div>
+										<div class="col-xl-6 col">
+										<div class="btn btn-primary btn-sm ">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										Add
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xl-3 col-md-3 col-lg-3 col-sm-3">
+							<a href="#FIXME" title="Strawberry">
+								<div class="card_content">
+									<figure>
+										<img src="assets/image/vegitable/vegi_one.jpg" alt="Vegitable One">
+									</figure>
+									<h6>strawberry</h6>
+									<p>Lorem ipsum dolor sit amet.</p>
+									<div class="form-group">
+										<select id="inputState" class="form-control form-control-sm">
+											<option selected>Choose...</option>
+											<option>500g - Rs.17.50</option>
+											<option>1kg - Rs.33.00</option>
+											<option>2Kg - Rs.66.00</option>
+										</select>
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="inlineFormInputGroup">Username</label>
+										<div class="input-group mb-2">
+											<div class="input-group-prepend">
+												<div class="input-group-text form-control-sm" style="font-size:14px">Qty</div>
+											</div>
+											<input type="Number" class="form-control form-control-sm" id="inlineFormInputGroup" placeholder="1">
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xl-6 col">
+											<div class="btn btn-primary btn-sm view_details">
+											 Details
+											</div>
+										</div>
+										<div class="col-xl-6 col">
+										<div class="btn btn-primary btn-sm ">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										Add
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xl-3 col-md-3 col-lg-3 col-sm-3">
+							<a href="#FIXME" title="Strawberry">
+								<div class="card_content">
+									<figure>
+										<img src="assets/image/vegitable/vegi_two.jpg" alt="Vegitable One">
+									</figure>
+									<h6>strawberry</h6>
+									<p>Lorem ipsum dolor sit amet.</p>
+									<div class="form-group">
+										<select id="inputState" class="form-control form-control-sm">
+											<option selected>Choose...</option>
+											<option>500g - Rs.17.50</option>
+											<option>1kg - Rs.33.00</option>
+											<option>2Kg - Rs.66.00</option>
+										</select>
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="inlineFormInputGroup">Username</label>
+										<div class="input-group mb-2">
+											<div class="input-group-prepend">
+												<div class="input-group-text form-control-sm" style="font-size:14px">Qty</div>
+											</div>
+											<input type="Number" class="form-control form-control-sm" id="inlineFormInputGroup" placeholder="1">
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xl-6 col">
+											<div class="btn btn-primary btn-sm view_details">
+											 Details
+											</div>
+										</div>
+										<div class="col-xl-6 col">
+										<div class="btn btn-primary btn-sm ">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										Add
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xl-3 col-md-3 col-lg-3 col-sm-3">
+							<a href="#FIXME" title="Strawberry">
+								<div class="card_content">
+									<figure>
+										<img src="assets/image/vegitable/vegi_three.jpg" alt="Vegitable One">
+									</figure>
+									<h6>strawberry</h6>
+									<p>Lorem ipsum dolor sit amet.</p>
+									<div class="form-group">
+										<select id="inputState" class="form-control form-control-sm">
+											<option selected>Choose...</option>
+											<option>500g - Rs.17.50</option>
+											<option>1kg - Rs.33.00</option>
+											<option>2Kg - Rs.66.00</option>
+										</select>
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="inlineFormInputGroup">Username</label>
+										<div class="input-group mb-2">
+											<div class="input-group-prepend">
+												<div class="input-group-text form-control-sm" style="font-size:14px">Qty</div>
+											</div>
+											<input type="Number" class="form-control form-control-sm" id="inlineFormInputGroup" placeholder="1">
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xl-6 col">
+											<div class="btn btn-primary btn-sm view_details">
+											 Details
+											</div>
+										</div>
+										<div class="col-xl-6 col">
+										<div class="btn btn-primary btn-sm ">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										Add
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-xl-3 col-md-3 col-lg-3 col-sm-3">
+							<a href="#FIXME" title="Strawberry">
+								<div class="card_content">
+									<figure>
+										<img src="assets/image/vegitable/vegi_four.jpg" alt="Vegitable One">
+									</figure>
+									<h6>strawberry</h6>
+									<p>Lorem ipsum dolor sit amet.</p>
+									<div class="form-group">
+										<select id="inputState" class="form-control form-control-sm">
+											<option selected>Choose...</option>
+											<option>500g - Rs.17.50</option>
+											<option>1kg - Rs.33.00</option>
+											<option>2Kg - Rs.66.00</option>
+										</select>
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="inlineFormInputGroup">Username</label>
+										<div class="input-group mb-2">
+											<div class="input-group-prepend">
+												<div class="input-group-text form-control-sm" style="font-size:14px">Qty</div>
+											</div>
+											<input type="Number" class="form-control form-control-sm" id="inlineFormInputGroup" placeholder="1">
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xl-6 col">
+											<div class="btn btn-primary btn-sm view_details">
+												Details
+											</div>
+										</div>
+										<div class="col-xl-6 col">
+										<div class="btn btn-primary btn-sm ">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										Add
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+
+					</div>
+				</div>
+		</div>
+		<?php include('footer.php')?>
+	</div>
+</section>
+
+
+</main>
+
+
+
