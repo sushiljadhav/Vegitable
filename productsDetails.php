7dc6f8e19f9ba68a8f35088675c11e76277e95e8
sushiljadhav <67819154+sushiljadhav@users.noreply.github.com>
2020-12-16 21:18:30 +0530

    Add contain in ProductsDetails Page

--- productsDetails.php
+++ productsDetails.php
@@ -15,7 +15,56 @@
 
 <section class="products_details">
 	<div class="container">
-		 	
+		<div class="row">
+			<div class="col-md-3">
+			<div class="row">
+				<div class="col-md-1">
+					<ul class="row products_images_slider">
+						<li>
+							<figure>
+							<img src="assets/image/vegitable/vegi_one.jpg" alt="Vegitable One">
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="assets/image/vegitable/vegi_two.jpg alt="Vegitable two">
+							</figure>
+						</li>
+						<li>
+							<figure>
+							<img src="assets/image/vegitable/vegi_three.jpg alt="Vegitable three">
+							</figure>
+						</li>
+						<li>
+							<figure>
+							<img src="assets/image/vegitable/vegi_Four.jpg alt="Vegitable Four">
+							</figure>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-2">
+					<figure>
+						<img src="assets/image/vegitable/vegi_one.jpg" alt="Vegitable One">
+					</figure>
+					<div class="row products_details_button">
+						<div class="col">
+							<button class="btn btn-primary">
+								Add To cart
+							</button>
+						</div>
+						<div class="col">
+							<button class="btn btn-primary">
+								Buy Now
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+				
+			</div>
+			<div class="col-md-9">
+			</div>
+		</div>
 	</div>
 
 </section>
